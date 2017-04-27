ac74267501216a6dbbe5d0f377566e7c1299ca77
Shayan aminnjad <shayanaminnjad.sa@gmail.com>
2017-04-27 16:45:11 +0430

    Create main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,49 @@
+#include "World.h"
+#include "LogHandler.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include <iostream>
+
+
+#if 1
+int main()
+{
+	sf::Time frameRate = sf::seconds(1.f / 30.f);
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Snake Eater");
+	sf::View myView = window.getDefaultView();
+	LogHandler timeLog("frameRate.txt");
+	window.setVerticalSyncEnabled(false);
+	window.setKeyRepeatEnabled(false);
+
+	sf::Clock clock;
+	sf::Time deltaTime= sf::Time::Zero;
+
+	World world(window);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		deltaTime += clock.restart();
+		
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			world.handleEvent(event);
+		}
+
+		
+		while (deltaTime >= frameRate)
+		{
+			world.update(frameRate);
+			deltaTime -= frameRate;
+		}
+
+		world.render();
+	}
+}
+
+#endif
